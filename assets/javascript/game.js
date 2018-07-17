40311d285e3c4ae09d88b5811cd7a16c92bdbdd5
Fearhad <allan.hartigan@gmail.com>
2018-07-17 09:31:41 -0400

    removing web audio api

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,3 @@
-var audio = new Audio();
-audio.src = 'assets/music/OttoHalmén-SylvanWaltz.mp3';
-audio.controls = true;
-audio.autoplay = true;
-document.body.appendChild(audio);
-
-var context = new AudioContext();
-var analyser = context.createAnalyser();
-
-
-window.addEventListener('load', function(e) {
-  // Our <audio> element will be the audio source.
-  var source = context.createMediaElementSource(audio);
-  source.connect(analyser);
-  analyser.connect(context.destination);
-
-
-}, false);
 
 /**
  * Load the main menu
